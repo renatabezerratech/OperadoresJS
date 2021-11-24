01695edeec6a8be15d8a3c14aba7f99388e3eb74
renatabezerratech <renatabezerratecnologia@gmail.com>
2021-11-23 23:23:54 -0300

    add comment

--- operadores.js
+++ operadores.js
@@ -3,11 +3,11 @@
 let frase = 'Roma me tem amor';
 let aux = frase;
 
-frase = frase.toLocaleLowerCase();
-frase = frase.split(" ").join("");
+frase = frase.toLocaleLowerCase(); // Métodos da string
+frase = frase.split(" ").join("");  // ['roma','me','tem','amor'] 'romametemamor'
 
-let frase_invertida = frase.split("").reverse().join("");
-frase_invertida = frase_invertida.split(" ").join("");
+let frase_invertida = frase.split("").reverse().join(""); // 'romametemamor' - invertido
+// frase_invertida = frase_invertida.split(" ").join("");
 
 if (frase == frase_invertida) {
   alert("A " + aux + " é uma palíndromo");
